5248413bad04c7f013f7c926e7f0509e645a1827
Falco <falco@falco.in>
2020-03-07 15:57:46 +0100

    test file (unittest)

---
+++ tests/test_myCloud.py
@@ -0,0 +1,44 @@
+import unittest
+
+from cloudapi import cloudovh
+
+
+class TestsOVH(unittest.TestCase):
+    def setUp(self):
+        self.ovh = cloudovh.MyCloud(config_file="../ovh.conf")
+
+    def isNotEmptyList(self, l):
+        self.assertIsInstance(l, list)
+        self.assertTrue(l)
+
+    def test___init__(self):
+        self.assertIsNotNone(cloudovh.CONFIG_PATH)
+        for s in (self.ovh._projectname,
+                  self.ovh._sshKeyId,
+                  self.ovh._serviceName):
+            self.assertIsInstance(s, str)
+            self.assertTrue(s)
+
+    def test_show_ip(self):
+        r = self.ovh.show_ip()
+        self.assertIsInstance(r, list)
+        self.assertIsInstance(r[0], dict)
+
+    def test_list_instances(self):
+        self.isNotEmptyList(self.ovh.list_instances())
+
+    def test_list_volumes(self):
+        self.isNotEmptyList(self.ovh.list_volumes())
+
+    def test_filter(self):
+        flavor = self.ovh._default_flavor
+        image = self.ovh._default_image
+        region = self.ovh._default_region
+        self.assertIs(1, len(self.ovh.flavor(
+            filter={'name': flavor, 'region': region})))
+        self.assertIs(1, len(self.ovh.image(
+            filter={'name': image, 'region': region})))
+
+
+if __name__ == '__main__':
+    unittest.main()
